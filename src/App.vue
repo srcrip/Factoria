ff944d08655e457605ad6ac42d60b5b7db6b0984
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-03-20 18:03:58 -0400

    Setup navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app.has-background-black
+    //- #nav
+    //-   router-link(to="/") Home
+    //-   p |
+    //-   router-link(to="/about") About
+    navbar
+    .container
+      section.section
+        router-view
 </template>
 
-<style lang="scss">
-#app {
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  components: {
+    navbar: () => import('@/components/Navbar.vue'),
+  },
+
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="sass">
+@import "@/assets/sass/_variables.sass"
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#nav
+  padding: 30px
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  a
+    font-weight: bold
+    color: #2c3e50
+
+    &.router-link-exact-active
+      color: #42b983
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  b-navbar(type='is-black')
+    template(slot='brand')
+      b-navbar-item(tag='router-link' :to="{ path: '/' }") Factoria
+    template(slot='start')
+      b-navbar-item(href='#')
+        | Home
+      b-navbar-item(href='#')
+        | Documentation
+      b-navbar-dropdown(label='Info')
+        b-navbar-item(href='#')
+          | About
+        b-navbar-item(href='#')
+          | Contact
+    template(slot='end')
+      b-navbar-item(tag='div')
+        .buttons
+          a.button.is-primary
+            strong Sign up
+          a.button.is-light
+            | Log in
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped lang="sass">
+@import "@/assets/sass/_variables.sass"
+</style>
