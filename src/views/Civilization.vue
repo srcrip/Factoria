8bc9ce51aefe2dccaeea8e1bc54bf655944720bd
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-03-20 18:12:08 -0400

    Change the default component

---
+++ src/views/Civilization.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  p Stone: {{ stone }}
+</template>
+
+<script>
+export default {
+  name: 'Civilization',
+  data () {
+    return {
+      stone: 0,
+    }
+  },
+}
+</script>
